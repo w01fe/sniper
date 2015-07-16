c9b03528b509dc37aeaeb5fe77f7fe59bd874873
Jason Wolfe <jason@w01fe.com>
2015-07-15 21:17:38 -0700

    Add project skeleton

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject sniper "0.1.0-SNAPSHOT"
+  :description "Snipe at dead code"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
---
+++ src/sniper/core.clj
@@ -0,0 +1,1 @@
+(ns sniper.core)
