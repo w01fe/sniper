cdd83560638f6dc8a999d06adbe35aae9caa48f3
Jason Wolfe <jason@w01fe.com>
2015-07-18 02:05:34 -0700

    WIP

--- src/sniper/snarf.clj
+++ src/sniper/snarf.clj
@@ -9,6 +9,7 @@
    [clojure.tools.analyzer.jvm.utils :as jvm-utils]
    [clojure.tools.analyzer.passes.jvm.validate :as jvm-val]
    [clojure.tools.analyzer.ast :as ast]
+   [clojure.tools.namespace.find :as namespace-find]
    [clojure.tools.reader :as reader]
    [clojure.tools.reader.reader-types :as reader-types]
    [schema.core :as s]
@@ -18,30 +19,30 @@
 ;; Copied from tools.analyzer.jvm master for EOF fix.
 ;; also modified to take file.
 
-(defn analyze-url
-  "Analyzes a url, returns a vector of the ASTs for all the
+
+(defn analyze-ns
+  "Analyzes a whole namespace. returns a vector of the ASTs for all the
    top-level ASTs of that file.
    Evaluates all the forms.
-   Disables wrong-tag-handler, and fixes bug with opts shadowing.
-"
-  ([url] (println "ZZZZ") (analyze-url url (jvm/empty-env)))
-  ([url env] (analyze-url url env {:passes-opts
-                                   (merge
-                                    jvm/default-passes-opts
-                                    {:validate/wrong-tag-handler
-                                     (fn [_ ast]
-                                       (println "Wrong tag: " (-> ast :name meta :tag)
-                                                " in def: " (:name ast)))})}))
-  ([^java.net.URL url env opts]
-     (println "AAA")
-     (println "Analyzing file"  #_(.getPath url))
+   Disables wrong-tag-handler, and fixes bug with opts shadowing,
+   and doesn't eval."
+  ([ns] (analyze-ns ns (jvm/empty-env)))
+  ([ns env] (analyze-ns ns env {:passes-opts
+                                (merge
+                                 jvm/default-passes-opts
+                                 {:validate/wrong-tag-handler
+                                  (fn [_ ast]
+                                    #_(println "Wrong tag: " (-> ast :name meta :tag)
+                                               " in def: " (:name ast)))})}))
+  ([ns env opts]
+     (println "Analyzing ns" ns)
      (env/ensure (jvm/global-env)
-                 (let [res url]
-                   (assert res (str "Can't find " url " in classpath"))
+                 (let [res ^java.net.URL (jvm-utils/ns-url ns)]
+                   (assert res (str "Can't find " ns " in classpath"))
                    (let [filename (str res)
                          path     (.getPath res)]
                      (when-not (get-in (env/deref-env) [::analyzed-clj path])
-                       (binding [*ns*   (the-ns 'api.deploy) #_*ns*
+                       (binding [*ns*   (the-ns ns)
                                  *file* filename]
                          (with-open [rdr (java-io/reader res)]
                            (let [pbr (reader-types/indexing-push-back-reader
@@ -56,22 +57,7 @@
                                  (if (identical? form eof)
                                    ret
                                    (recur
-                                    (conj ret (jvm/analyze form (assoc env :ns (ns-name *ns*)) opts))))))
-                             #_(loop []
-                                 (let [form (reader/read read-opts pbr)]
-                                   (when-not (identical? form eof)
-                                     (swap! env/*env* update-in [::analyzed-clj path]
-                                            (fnil conj [])
-                                            (jvm/analyze+eval form (assoc env :ns (ns-name *ns*)) opts))
-                                     (recur))))))))
-                     #_(get-in @env/*env* [::analyzed-clj path]))))))
-
-(defn analyze-ns
-  "Analyzes a whole namespace."
-  ([ns] (analyze-url (jvm-utils/ns-url ns)))
-  ([ns env] (analyze-url (jvm-utils/ns-url ns) env))
-  ([ns env opts]
-     (analyze-url (jvm-utils/ns-url ns))))
+                                    (conj ret (jvm/analyze form (assoc env :ns (ns-name *ns*)) opts)))))))))))))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -129,7 +115,8 @@
   (let [nodes (ast/nodes ast-node)
         unique (fn [f] (sort-by str (distinct (mapcat f nodes))))]
     (if-not (get-in ast-node [:env :line])
-      (do (assert (= (ffirst (:raw-forms ast-node)) 'comment))
+      (do (assert (= (ffirst (:raw-forms ast-node)) 'comment)
+                  (str "MISSING line" (:raw-forms ast-node) (:env ast-node)))
           nil)
       {:source-info (select-keys (:env ast-node) [:file :ns :line :column :end-column :end-line])
        :class-defs (unique class-defs)
@@ -140,60 +127,40 @@
 
 (s/defn ns-forms :- [sniper/Form]
   [& nss :- [clojure.lang.Symbol]]
-  (keep normalized-form (mapcat analyze-ns nss)))
-
-(s/defn file-forms :- [sniper/Form]
-  [& fs :- [String]]
-  (keep normalized-form (mapcat #(analyze-url (java.net.URL. (str "file://" %))) fs)))
+  (keep normalized-form (apply concat (map analyze-ns nss))))
 
 
-(defn files-in-dir
-  "Returns a sequence of namespaces defined in clj(s) files in directory including subdirs"
-  [base-dir]
-  (->> (file-seq (java-io/file base-dir))
-       (remove #(.isDirectory ^java.io.File %))
-       (filter #(re-find #"\.clj" (.getName ^java.io.File %)))))
-
-(defn ns-sym->filename [ns-sym]
-  (let [pubs (-> ns-sym the-ns ns-publics)
-        metas (map (comp meta val) pubs)
-        ns-metas (filter #(when-let [ns (:ns %)] (= (ns-name ns) ns-sym)) metas)
-        f (first (keep :file ns-metas))]
-    (when-not f
-      (throw (RuntimeException. (format "Couldn't find file for ns %s" ns-sym))))
-    f))
-
-(defn loaded-namespaces [file-regex]
-  (filter #(re-find file-regex (ns-sym->filename %)) (map ns-name (all-ns))))
-
-(defn resolve-filename [^String f]
-  (println f)
-  (if (.startsWith f "/")
-    f
-    (.getFile (.getResource (clojure.lang.RT/baseLoader) f))))
-
-(defn loaded-clojure-files [file-regex]
-  (->> (all-ns)
-       (mapcat #(vals (ns-map %)))
-       (keep #(:file (meta %)))
-       (remove (fn [^String f] (or (= "NO_SOURCE_FILE" f) (.startsWith f "jar:file:"))))
-       distinct
-       (map resolve-filename)
-       (filter #(re-find file-regex %))))
-
-(defn classpath-clojure-files [file-regex]
+(defn classpath-namespaces [dir-regex]
   (->> (classpath/classpath)
        (mapcat file-seq)
        distinct
-       (remove #(.isDirectory ^java.io.File %))
-       (map #(.getPath ^java.io.File %))
-       (filter #(and (re-find file-regex %) (.endsWith ^String % ".clj")))))
+       (filter (fn [^java.io.File f]
+                 (and (.isDirectory f)
+                      (re-find dir-regex (.getPath f)))))
+       (mapcat #(namespace-find/find-namespaces-in-dir %))
+       distinct
+       sort))
+
+(defn classpath-ns-forms [dir-regex]
+  (let [nss (classpath-namespaces dir-regex)]
+    (doseq [ns nss]
+      (println "Requiring" ns)
+      (require ns))
+    )
+  )
+
+(comment
+  (let [namespaces (sort )]
+    ;; (doseq [i init] (println "requiring" i) (require i))
+    (apply require namespaces)
+    (run-tests-selector test-selector (preprocess-nss namespaces))))
 
 ;; TODO: get all files on classpath including tests.
 
 ;; TODO: use ordinary require and no eval, just forget about non-ns files
 
 (comment
+  (sniper.snarf/namespaces #"/Users/w01fe/prismatic")
   (sniper.snarf/classpath-clojure-files #"^/Users/w01fe/prismatic")
   (doseq [f (sniper.snarf/classpath-clojure-files #"^/Users/w01fe/prismatic")] (try (load-file f) (catch Exception e)))
   )
